81266b656c1adc1cced6e3cf802eebcbacc73dfb
ttymed <tyler.boright@gmail.com>
2017-08-19 10:13:27 -0700

    adds a method for IsInGraph and is looping through the linked list

--- graphs/isInGraph/isInGraph.go
+++ graphs/isInGraph/isInGraph.go
@@ -7,7 +7,6 @@ import (
 func BuildLL(strs []string) *List {
 	list := NewList()
 	for idx, _ := range strs {
-		fmt.Println(string(strs[idx]))
 		el := Element{}
 		el.Val = string(strs[idx])
 		list.InsertAtEnd(&el)
@@ -15,10 +14,18 @@ func BuildLL(strs []string) *List {
 	return list
 }
 
+func IsInGraph(str string, l *List) (foundStr bool) {
+	strHash := make(map[string]int)
+	fmt.Println(strHash)
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Val)
+	}
+	return
+}
+
 func main() {
 	strs1 := []string{"t", "X", "T", "x", "y", "s", "P", "x"}
 	// strs2 := []string{"b", "r", "e", "a", "k", "t", "h", "e", "s", "p", "e", "l", "l"}
-	//
 	ll := BuildLL(strs1)
-	fmt.Printf("%+v", ll)
+	IsInGraph("tyler", ll)
 }
