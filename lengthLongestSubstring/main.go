7a5ec0c94fb8197940a8dfbd1c5d1aa785a97d0e
Tyler Boright <lupin3.ken@gmail.com>
2018-09-12 21:18:38 -0700

    adds initial implementation of the algorithm

--- lengthLongestSubstring/main.go
+++ lengthLongestSubstring/main.go
@@ -3,10 +3,29 @@ package main
 import "fmt"
 
 func lengthOfLongestSubstring(s string) int {
+	seenHash := make(map[string]int)
+	currCount := 0
+	longestCount := 0
+	for _, char := range s {
+		strChar := string(char)
+		if _, ok := seenHash[strChar]; ok == false {
+			currCount += 1
+			seenHash[strChar] = 1
+		} else {
+			seenHash = make(map[string]int)
+			currCount = 1
+			seenHash[strChar] = 1
+		}
 
-	return 0
+		if currCount > longestCount {
+			longestCount = currCount
+		}
+	}
+
+	return longestCount
 }
 
 func main() {
 	fmt.Println("vim-go")
+	lengthOfLongestSubstring("abcabcabb")
 }
