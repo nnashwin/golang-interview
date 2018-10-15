a9c6962a80e381d87b9ff42c435ce8330b96c4f2
ru-lai <tyler.boright@gmail.com>
2018-10-15 12:02:04 -0700

    adds logic to handle 0 in the function

--- multiplyArraySubtractIndex/main.go
+++ multiplyArraySubtractIndex/main.go
@@ -1,16 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func multiplyArraySubtractIndex(input []int) []int {
 	total := 1
 
 	for i := 0; i < len(input); i++ {
-		total *= input[i]
+		if input[i] > 0 {
+			total *= input[i]
+		}
 	}
 
 	for i := len(input) - 1; i >= 0; i-- {
-		input[i] = total / input[i]
+		if input[i] > 0 {
+			input[i] = total / input[i]
+		} else {
+			input[i] = total
+		}
 	}
 
 	return input
@@ -18,5 +26,5 @@ func multiplyArraySubtractIndex(input []int) []int {
 
 func main() {
 	fmt.Println(multiplyArraySubtractIndex([]int{1, 2, 3, 4, 5}))
-	fmt.Println(multiplyArraySubtractIndex([]int{3, 2, 1}))
+	fmt.Println(multiplyArraySubtractIndex([]int{3, 2, 1, 0}))
 }
