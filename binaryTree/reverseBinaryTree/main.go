97796d54d0bea870a66888273ca360b315e3f348
ru-lai <tyler.boright@gmail.com>
2018-11-20 14:41:35 -0800

    adds algorithm to reverse the binary tree and check by running in-order dfs on the btree to see if the nodes are in correct order

--- binaryTree/reverseBinaryTree/main.go
+++ binaryTree/reverseBinaryTree/main.go
@@ -34,7 +34,49 @@ func createTree(keys []int) *BNode {
 	return root
 }
 
+func reverseBinaryTree(root *BNode) *BNode {
+	if root == nil {
+		return root
+	}
+
+	tmp := root.Right
+	root.Right = root.Left
+	root.Left = tmp
+
+	if root.Left != nil {
+		reverseBinaryTree(root.Left)
+	}
+
+	if root.Right != nil {
+		reverseBinaryTree(root.Right)
+	}
+
+	return root
+}
+
+func inOrder(root *BNode, sl *[]int) []int {
+	if root == nil {
+		return *sl
+	}
+
+	if root.Left != nil {
+		inOrder(root.Left, sl)
+	}
+
+	*sl = append(*sl, root.Val)
+
+	if root.Right != nil {
+		inOrder(root.Right, sl)
+	}
+
+	return *sl
+}
+
 func main() {
 	bst1 := createTree([]int{15, 10, 20, 8, 5, 2})
-	fmt.Println(bst1)
+	fmt.Println(inOrder(bst1, &[]int{}))
+	rbst := reverseBinaryTree(bst1)
+
+	fmt.Println("reverse starts here")
+	fmt.Println(inOrder(rbst, &[]int{}))
 }
