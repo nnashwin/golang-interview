b86f335d884c8fcbfe7e45e1e46306373e6175d5
ru-lai <tyler.boright@gmail.com>
2018-11-26 17:18:28 -0800

    adds implementation of returning the first nonDuplicate character

---
+++ strings/firstNonDuplicate/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func firstNonDuplicate(str string) (returnCh string) {
+	intervals := make(map[rune]int)
+
+	for _, c := range str {
+		intervals[c]++
+	}
+
+	for _, c := range str {
+		if intervals[c] == 1 {
+			returnCh = string(c)
+			return
+		}
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println(firstNonDuplicate("cacb"))
+	fmt.Println(firstNonDuplicate("bcbc"))
+}
