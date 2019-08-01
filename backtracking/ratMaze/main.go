296e99ee178084b534a81f752fd8b971daacecc9
ru-lai <tyler.boright@gmail.com>
2019-08-01 13:25:02 -0700

    master adds the ratMaze repo and a basic backtracking solution to solve the problem

---
+++ backtracking/ratMaze/main.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func buildMatrix(n int) (maze [][]int) {
+	for i := 0; i < n; i++ {
+		maze = append(maze, []int{})
+		for j := 0; j < n; j++ {
+			maze[i] = append(maze[i], 0)
+		}
+	}
+
+	return
+}
+
+func isSafe(maze [][]int, x int, y int) bool {
+	return x >= 0 && y >= 0 && x < len(maze) && y < len(maze) && maze[x][y] != 0
+}
+
+func findPath(maze [][]int, x int, y int, solution *[][]int) bool {
+	l := len(maze)
+
+	if x == l-1 && y == l-1 {
+		(*solution)[x][y] = 1
+		return true
+	}
+
+	if isSafe(maze, x, y) == true {
+		(*solution)[x][y] = 1
+		if findPath(maze, x+1, y, solution) {
+			return true
+		}
+
+		if findPath(maze, x, y+1, solution) {
+			return true
+		}
+
+		(*solution)[x][y] = 0
+		return false
+	}
+
+	return false
+}
+
+func ratInAMaze(maze [][]int) [][]int {
+	solution := buildMatrix(len(maze))
+
+	if findPath(maze, 0, 0, &solution) == true {
+		return solution
+	}
+
+	return nil
+}
+
+func main() {
+	maze := [][]int{[]int{1, 0, 0, 0},
+		[]int{1, 1, 1, 1},
+		[]int{0, 0, 1, 0},
+		[]int{0, 1, 1, 1}}
+
+	fmt.Println(ratInAMaze(maze))
+	fmt.Println("vim-go")
+}
