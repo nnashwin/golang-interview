28de8972d0ba42c6282575e7075868fede4a4055
ru-lai <ru.lai.development@gmail.com>
2020-03-12 00:37:14 -0700

    adds 3 implementations of the algorithm: one with two for loops, one removing an unnecessary for loop, and one that returns the indices of the maxSubArray

---
+++ arrays/findSubarrayWithMaximumSum/main.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func maximumSumInefficient(arr []int) int {
+	globalMax := 0
+	for i := 0; i < len(arr); i++ {
+		localMax := arr[i]
+		for j := i + 1; j < len(arr); j++ {
+			localMax = max(localMax+arr[j], arr[j])
+			globalMax = max(localMax, globalMax)
+		}
+	}
+
+	return globalMax
+}
+
+func maxSum(arr []int) int {
+	currentMax, maxEndingHere := 0, 0
+	for _, val := range arr {
+		maxEndingHere = max(maxEndingHere+val, val)
+		currentMax = max(maxEndingHere, currentMax)
+	}
+
+	return currentMax
+}
+
+func maximumSumWithIndices(arr []int) (int, []int) {
+	arrIdxs := []int{0, 0}
+	currentMax, maxEndingHere := 0, 0
+	for idx := 0; idx < len(arr); idx++ {
+		val := arr[idx]
+		if maxEndingHere+val < val {
+			arrIdxs[0] = idx
+			maxEndingHere = val
+		} else {
+			maxEndingHere = maxEndingHere + val
+		}
+		if maxEndingHere < currentMax {
+			arrIdxs[1] = idx - 1
+		} else {
+			currentMax = maxEndingHere
+			arrIdxs[1] = idx
+		}
+	}
+
+	return currentMax, arrIdxs
+}
+
+func main() {
+	fmt.Println(max(-3, 1))
+	fmt.Println(maxSum([]int{1, 3, 5, 7, 9}))
+	fmt.Println(maxSum([]int{1, -3, 2, 1, -1}))
+	fmt.Println(maxSum([]int{1, -3, 2, 1, -1}))
+	fmt.Println(maximumSumInefficient([]int{1, -3, 2, 1, -1}))
+	fmt.Println(maximumSumWithIndices([]int{1, -3, 2, 1, -1}))
+}
